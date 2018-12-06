8350aa05325f75f84d8548431b6ab5873558e6ff
LAPTOP-COK9CFVT\ssma1 <ssma100701@gmail.com>
2018-12-06 19:45:33 +1100

    update and delete request

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -89,7 +89,24 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'text' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else {
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+
+        }
     }
 
     /**
@@ -100,6 +117,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+        $item->delete();
+
+        $response = array('response' => 'Item deleted', 'success' => true);
+        return $response;
     }
 }
